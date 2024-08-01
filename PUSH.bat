97bc6c61d6522b1a90351af39a77ac1373833aa4
KaeyRiS <55554280+KaeyRiS@users.noreply.github.com>
2024-08-01 14:49:42 +0200

    Adding new items to current repository

--- PUSH.bat
+++
@@ -1,8 +0,0 @@
-E:
-cd E:\00_GitNuke
-git config --global core.autocrlf false
-git add .
-git commit -m "Adding new items to current repository"
-git push --set-upstream origin master -v
-git merge master
-pause
