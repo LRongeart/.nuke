1224d69faff5c9e6b10b8a1b3ff020ae2dcdaa11
KaeyRiS <55554280+KaeyRiS@users.noreply.github.com>
2024-11-14 21:26:46 +0100

    Adding new items to current repository

--- .nuke/HeatWave_v4.0/menu.py
+++
@@ -1,4 +0,0 @@
-# DAMIAN BINDER TOOLS
-toolbar = nuke.toolbar("Nodes")
-m = toolbar.addMenu("DamianBinder", icon="DamianBinderNukeLogo.png")
-m.addCommand("HeatWave", "nuke.createNode(\"HeatWave\")", icon="HeatWave_Icon.png")
--- PULL.bat
+++
@@ -1,7 +0,0 @@
-:: replace 'E:' by the disk letter on which your Git Folder exists; 
-E:
-:: replace 'E:\00_GitNuke' by the full disk path to your Git Folder;
-cd E:\00_GitTest
-git config --global core.autocrlf false
-git pull --rebase --autostash
-pause
